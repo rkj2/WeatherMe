e7e5325317a56ff04beb46365c1521c344f4a0bc
Rajive Jain <rajivejain@Rajives-MacBook-Pro.local>
2017-11-16 12:18:19 -0800

    Initial UI commit with view controller and subviews

---
+++ WeatherMe/Controllers/RKJHomeViewController.swift
@@ -0,0 +1,23 @@
+//
+//  RKJHomeViewController.swift
+//  WeatherMe
+//
+//  Created by Rajive Jain on 11/16/17.
+//  Copyright © 2017 Rajive Jain. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class RKJHomeViewController : UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+    
+    
+}
